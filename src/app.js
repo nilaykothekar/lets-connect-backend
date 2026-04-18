3c92b70ad73682a90c595c27ec5d9929b102c4f5
Nilay Kothekar <nilayvk@gmail.com>
2026-04-18 18:23:51 +0530

    added a post signup call

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 const express = require("express");
-const { connectDB } = require("../config/database");
+const { connectDB } = require("./config/database");
+const { User } = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName: "Sachin",
+        lastName: "Tendulkar",
+        email: "sachintendulkar@gmail.com",
+        password: "Sachin@1234",
+        age: 34,
+        gender: "Male"
+    };
+
+    const user = new User(userObj);
+
+    try {
+        await user.save();
+        res.send("User added successfully");
+    } catch (error) {
+        console.error("Error adding user:", error);
+        return res.status(400).send("Bad Request");
+    }
+});
 
 connectDB().then(() => {
     console.log("Connected to the database");
